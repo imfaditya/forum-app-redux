a0c4f95e24a90a241e997673a0a3ed51830fef10
Imam Faraz Aditya <imamaditya2211@gmail.com>
2023-03-01 20:39:49 +0700

    fix typo on reducer

--- src/states/detailThread/reducer.js
+++ src/states/detailThread/reducer.js
@@ -1,7 +1,7 @@
 import { ActionType } from './action';
 
 const detailThreadReducer = (detailThread = null, action = {}) => {
-  switch (action.payload.type) {
+  switch (action.type) {
     case ActionType.RECEIVE_DETAIL_THREAD:
       return action.payload.detailThread;
     case ActionType.CLEAR_DETAIL_THREAD:
