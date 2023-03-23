4998a119017081999d9704b82a1201ee0e3496e0
Imam Faraz Aditya <imamaditya2211@gmail.com>
2023-03-23 17:21:20 +0700

    add test for thunk function at auth user action

---
+++ src/states/authUser/action.test.js
@@ -0,0 +1,52 @@
+/**
+ * Test Scenario Set Auth User Thunk
+ *
+ * - asyncSetAuthUser Function
+ * - should dispatch action correctly when data fetchin success
+ * - should dispatch action and call alert correctly when data fetching failed
+ *
+*/
+
+import { hideLoading, showLoading } from 'react-redux-loading-bar';
+import api from '../../utils/api';
+import { asyncSetAuthUser, setAuthUserActionCreator } from './action';
+
+const fakeAuthUserResponse = {
+  id: 'john_doe',
+  name: 'John Doe',
+  email: 'john@example.com',
+  avatar: 'https://generated-image-url.jpg',
+};
+
+describe('asyncSetAuthUser thunk', () => {
+  beforeEach(() => {
+    api._getAuthUserProfile = api.getAuthUserProfile;
+    api._login = api.login;
+  });
+
+  afterEach(() => {
+    api.getAuthUserProfile = api._getAuthUserProfile;
+    api.login = api._login;
+
+    delete api._login;
+    delete api._getAuthUserProfile;
+  });
+
+  it('should dispatch action correctly when data fetchin success', async () => {
+    // Arrange
+    const email = 'tes@123.com';
+    const password = '123456';
+    api.login = () => Promise.resolve();
+    api.getAuthUserProfile = () => Promise.resolve(fakeAuthUserResponse);
+
+    const dispatch = jest.fn();
+
+    // Action
+    await asyncSetAuthUser({ email, password })(dispatch);
+
+    // Assert
+    expect(dispatch).toHaveBeenCalledWith(showLoading());
+    expect(dispatch).toHaveBeenCalledWith(setAuthUserActionCreator(fakeAuthUserResponse));
+    expect(dispatch).toHaveBeenCalledWith(hideLoading());
+  });
+});
